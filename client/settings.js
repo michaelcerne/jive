361d8bad6762396ea415771c2ec6ae0a9ce53297
michaelcerne <michaelcerne1@gmail.com>
2017-11-07 19:26:20 -0500

    Converted settings hex code to color name via NTC

--- client/settings.js
+++ client/settings.js
@@ -1,14 +1,26 @@
+import { Tracker } from 'meteor/tracker';
+
 import './settings.html';
+import { ntc } from './ntc.js';
+
+const userColorDep = new Tracker.Dependency;
 
 Template.settings.helpers({
 	'sent': function(){
-		return Meteor.users.findOne({"_id":Meteor.userId()})['sent']
+		if(Meteor.user()) {
+			return Meteor.user().sent
+		}
 	},
 	'username': function(){
-		return Meteor.users.findOne({"_id":Meteor.userId()})['username']
+		if(Meteor.user()) {
+			return Meteor.user().username
+		}
 	},
 	'color': function() {
-    return Meteor.users.findOne({_id:Meteor.userId()})["color"]
+		if(Meteor.user()) {
+			userColorDep.depend();
+    	return ntc.name(Meteor.user().color)[1]
+  	}
   },
 });
 
@@ -16,6 +28,7 @@ Template.settings.events({
 	'submit #colorf': function(event) {
     event.preventDefault();
     var val = document.getElementById('colorval').value;
-    Meteor.call('userColor', val)
+    Meteor.call('userColor', val);
+  	userColorDep.changed()
   }
 })
